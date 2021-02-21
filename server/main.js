6b6fbefaace9b4c886912e2a3fc4582f3dcff2c9
o-star <uh04049@naver.com>
2021-02-21 20:27:19 +0900

    mongodb connect

--- server/main.js
+++ server/main.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 5000
 
 const api = require('./route/APIrouter')
+
+mongoose.connect('mongodb://127.0.0.1:27017',
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+    .then(() => console.log("MongoDB Connect Complete"))
+    .catch(err => console.log(`Mongo Error : ${err}`))
 
 app.use('/api', api);
 
